43a388f02392a77303cf062cefb66033a1bfbe99
Yechen Fan <yechenfan3@gmail.com>
2026-04-12 19:30:34 +0800

    Update setup.py

--- third_party/oculus_reader/setup.py
+++ third_party/oculus_reader/setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 
-
 setup(
-    name='oculus_reader',
-    version='1.0.0',
-    packages=['oculus_reader'],
-    license='Apache-2.0 License',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'numpy', 'pure-python-adb'
-    ],
-    package_data={'': ['APK/teleop-debug.apk']},
+    name="oculus_reader",
+    version="1.0.0",
+    packages=["oculus_reader"],
+    license="Apache-2.0 License",
+    long_description=open("README.md").read(),
+    install_requires=["numpy", "pure-python-adb"],
+    package_data={"": ["APK/teleop-debug.apk"]},
     include_package_data=True,
 )
